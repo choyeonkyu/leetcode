ad5a4d91807b66c1a10f38a70d59784f4957cc18
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2025-10-09 21:11:14 +0900

    Time: 75 ms (78.08%), Space: 13.4 MB (53.4%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -15,11 +15,11 @@ class Solution(object):
             return False
         if self.isSameTree(root, subRoot):
             return True
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) # DFS로 왼쪽 트리부터 서브트리 찾고 같은지 체크
     
     def isSameTree(self, a, b):
-        if not a and not b:
+        if not a and not b: # 둘 다 None인 경우 같은 거로 판단
             return True
-        if not a or not b:
+        if not a or not b: # 둘 중 하나만 None인 경우는 다른거로 판단
             return False
-        return (a.val == b.val and self.isSameTree(a.left, b.left) and self.isSameTree(a.right, b.right))
+        return (a.val == b.val and self.isSameTree(a.left, b.left) and self.isSameTree(a.right, b.right)) # 재귀 함수
