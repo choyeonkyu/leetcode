51acc5299af1a1bbb9281dbbe3cd9a510890a870
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2025-10-15 20:50:13 +0900

    Time: 109 ms (70.08%), Space: 20.8 MB (55.27%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -6,14 +6,13 @@ class Solution(object):
         """
         # 더 낮은쪽을 안쪽으로 이동
         start, end = 0, len(height)-1
-        answer = []
+        cur = (end - start) * min(height[start], height[end])
         while start < end:
-            cur = (end - start) * min(height[start], height[end])
-            answer.append(cur)
+            cur = max(cur , ((end - start) * min(height[start], height[end])))
             if height[start] < height[end]:
                 start += 1
             elif height[start] > height[end]:
                 end -= 1
             else:
                 start += 1
-        return max(answer)
+        return cur
