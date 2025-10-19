27f1807c1d1e5f1cd318d09619bd8a199698eff1
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2025-10-19 14:18:21 +0900

    Time: 117 ms (16.5%), Space: 12.5 MB (82.8%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,16 +7,9 @@ class Solution(object):
         from collections import deque
         ans = deque()
         for i in s:
-            # if i not in ans:
-            #     ans.append(i)
-            # else:
-            #     ans.popleft()
-            #     ans.append(i)
             ans.append(i)
             if len(list(set(ans))) == len(ans):
                 continue
             else:
                 ans.popleft()
-            print(ans)
         return len(ans)
-            # print(help(ans)) # append, appendleft, pop, popleft
