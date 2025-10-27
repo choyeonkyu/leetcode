62aff506d1bb0e6c03c8cc31474dc88acdbe3b42
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2025-10-27 22:47:23 +0900

    Time: 709 ms (9.05%), Space: 12.6 MB (97.59%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -4,9 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # import itertools
-        # print(help(itertools))
-        # ''''chain', 'combinations', 'combinations_with_replacement', 'compress', 'count', 'cycle', 'dropwhile', 'groupby', 'ifilter', 'ifilterfalse', 'imap', 'islice', 'izip', 'izip_longest', 'permutations', 'product', 'repeat', 'starmap', 'takewhile', 'tee''''
         from itertools import permutations
         x = permutations(nums, len(nums))
         ans = []
