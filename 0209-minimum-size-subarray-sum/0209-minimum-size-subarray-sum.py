28882aa29ebcb61c0a545e6d3a946345354c5931
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2025-11-20 21:02:19 +0900

    Time: 11 ms (95.06%), Space: 19.5 MB (75.55%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right, total_sum = 0, 0, 0
+        answer = 10**10
+        # if len(nums) >= 1:
+        #     right += 1
+        #     total_sum += nums[0]
+        while right >= left:
+            # print(left, right, nums[left:right], total_sum)
+            if total_sum >= target:
+                answer = min(answer, right-left)
+                total_sum -= nums[left]
+                left += 1
+            elif right < len(nums):
+                total_sum += nums[right]
+                right += 1 
+            else:
+                if answer == 10**10:
+                    return 0
+                else:
+                    return answer
+        
