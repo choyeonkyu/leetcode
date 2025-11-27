f277db59b74195a592f7291bcfc361da84b9ddaa
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2025-11-27 19:01:15 +0900

    Time: 51 ms (8.8%), Space: 13.3 MB (87.39%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        adj = [[] for _ in range(numCourses)]
+        in_degree = [0]*numCourses
+        for prev, cur in prerequisites:
+            adj[cur].append(prev)
+            in_degree[prev] += 1
+        print(adj, in_degree)
+        q = collections.deque()
+        for i in range(len(in_degree)):
+            if in_degree[i] == 0:
+                q.append(i)
+        while q:
+            cur = q.popleft()
+            for i in adj[cur]:
+                in_degree[i] -= 1
+                if in_degree[i] == 0:
+                    q.append(i)
+        return True if sum(in_degree) == 0 else False
