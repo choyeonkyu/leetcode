065acc0939b92d63c79b5c68a230f5e1138b2312
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2025-12-13 13:45:42 +0900

    Time: 0 ms (100%), Space: 13.7 MB (47.25%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -10,10 +10,12 @@ class Solution(object):
         answer = 0
         for i in range(len(prices)-1):
             if prices[i] < prices[i+1]:
-                if i+1 == len(prices)-1:
-                    answer += prices[i+1] - prices[left]
+                # if i+1 == len(prices)-1:
+                #     answer += prices[i+1] - prices[left]
                 continue
             else:
                 answer += prices[i] - prices[left]
                 left = i+1
+        if prices[i] < prices[i+1]:
+            answer += prices[i+1] - prices[left]
         return answer
