15a9ea11aa6173774db25c2215c88f72480201aa
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2025-12-14 10:25:08 +0900

    Time: 8 ms (80.32%), Space: 17 MB (32.97%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -9,13 +9,11 @@ class Solution(object):
         temp_left, temp_right = intervals[0][0], intervals[0][1]
         for (left, right) in intervals:
             if temp_right < left:
-                print('if', left, right)
                 answer.append([temp_left, temp_right])
                 temp_left, temp_right = left, right
-            elif temp_left <= left and temp_right >= right:
-                print('elif 1',left, right)
+            # elif temp_left <= left and temp_right >= right:
+            #     print('elif 1',left, right)
             elif temp_left <= left and temp_right < right: 
-                print('elif 2',left, right)
                 temp_right = right
         answer.append([temp_left, temp_right])
         return answer
