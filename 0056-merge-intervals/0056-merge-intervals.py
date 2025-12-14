b9123501e767bd4b590a749ccb3f772fe91a66c4
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2025-12-14 10:25:19 +0900

    Time: 11 ms (55.13%), Space: 17.1 MB (24.85%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -11,8 +11,6 @@ class Solution(object):
             if temp_right < left:
                 answer.append([temp_left, temp_right])
                 temp_left, temp_right = left, right
-            # elif temp_left <= left and temp_right >= right:
-            #     print('elif 1',left, right)
             elif temp_left <= left and temp_right < right: 
                 temp_right = right
         answer.append([temp_left, temp_right])
