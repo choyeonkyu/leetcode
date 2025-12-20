e73b1ab94dfecf7f24a6ea729f42292f9bfb4405
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2025-12-20 14:14:19 +0900

    Time: 11 ms (9.5%), Space: 12.5 MB (50.13%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        def calc(num):
+            temp = []
+            div = 10
+            while num != 0:
+                temp.append((num%div)**2)
+                num //= div
+            result = sum(temp)
+            if result < 10 and (result != 1 and result != 7):
+                result = False
+            return result
+        while n != 1:
+            temp_n = calc(n)
+            print(temp_n)
+            if temp_n == 1 or temp_n == 7:
+                return True
+            if not temp_n:
+                return False
+            n = temp_n
+        return True
