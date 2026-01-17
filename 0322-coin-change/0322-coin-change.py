a53a57b8c35e1052737e6151c072ce31b21d548c
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2026-01-17 18:38:40 +0900

    Time: 925 ms (37.79%), Space: 13 MB (83.5%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -5,22 +5,10 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        memo = {}
-        def dfs(remain):
-            if remain < 0:
-                return float('inf')
-            elif remain == 0:
-                return 0
-            if remain in memo:
-                return memo[remain]
-            
-            best = float('inf')
-            for i in coins:
-                cand = dfs(remain - i) + 1
-                best = min(cand, best)
-
-            memo[remain] = best
-            return best
-        
-        answer = dfs(amount)
-        return answer if answer != float('inf') else -1
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount + 1):
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], dp[i-c] + 1)
+        return dp[amount] if dp[amount] != (amount + 1) else -1
