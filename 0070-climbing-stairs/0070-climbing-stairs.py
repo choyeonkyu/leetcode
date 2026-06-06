223c5c30a5804063a9254b6250a7b50f2bc80c78
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2026-06-06 20:26:35 +0900

    Time: 0 ms (100%), Space: 12.4 MB (20.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,28 +1,18 @@
-import itertools
-from itertools import permutations, combinations
 class Solution(object):
     def climbStairs(self, n):
         """
         :type n: int
         :rtype: int
         """
-        def factorial(x):
-            if x == 0:
-                return 0
-            ret = 1
-            for num in range(1, x+1):
-                ret *= num
-            return ret
-        # print(factorial(2), factorial(3))
-        min_num = n // 2
-        answer = 0
-        for i in range(0, min_num+1):
-            print(answer)
-            num_2 = i
-            num_1 = (n-(2*i))
-            print(num_1, num_2)
-            if num_2 == 0 or num_1 == 0:
-                answer += 1
-                continue
-            answer += factorial(num_1 + num_2) / (factorial(num_1) * factorial(num_2))
-        return answer
+        # kind of decision tree approach
+        dp = {} # total -> # of ways
+
+        def backtrack(total):
+            if total >= n: # count
+                return 1 if total == n else 0
+            if total in dp: # memorization
+                return dp[total]
+            
+            dp[total] = backtrack(total + 1) + backtrack(total + 2)
+            return dp[total]
+        return backtrack(0)
