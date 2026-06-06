6738cd4bc26529e30a5d830e435205e15f27f84e
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2026-06-06 19:44:41 +0900

    Time: 4 ms (82.16%), Space: 12.5 MB (57.03%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,18 +4,32 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def addTwoNumbers(self, l1, l2, carry = 0):
+    def addTwoNumbers(self, l1, l2):
         """
         :type l1: Optional[ListNode]
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        if not l1 and not l2 and carry == 0:
-            return None
-        v1 = l1.val if l1 else 0
-        v2 = l2.val if l2 else 0
-        answer = ListNode()
-        cur_sum = v1 + v2 + carry
-        answer.val = cur_sum % 10
-        answer.next = self.addTwoNumbers(l1.next if l1 else None , l2.next if l2 else None , cur_sum // 10)
+        head = answer = ListNode()
+        cur1, cur2 = l1, l2
+        upper = 0
+        while cur1 or cur2:
+            if cur1 and cur2:
+                temp_val = cur1.val + cur2.val + upper
+            elif cur1:
+                temp_val = cur1.val + upper
+            elif cur2:
+                temp_val = cur2.val + upper
+            upper = 1 if temp_val > 9 else 0
+            temp_val = temp_val - 10 if upper == 1 else temp_val
+            head.val = temp_val
+            
+            cur1 = cur1.next if cur1 else None
+            cur2 = cur2.next if cur2 else None
+
+            if cur1 or cur2:
+                head.next = ListNode()
+                head = head.next
+            elif upper == 1:
+                head.next = ListNode(1)
         return answer
