c1c149096c4b35584a650c59e9a148f6ecd6593d
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2026-06-07 19:42:05 +0900

    Time: 7 ms (61.83%), Space: 12.5 MB (44.05%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,23 +1,17 @@
 class Solution(object):
-    
     def wordBreak(self, s, wordDict):
         """
         :type s: str
         :type wordDict: List[str]
         :rtype: bool
         """
-        self.memo = {}
-        return self.find(s, wordDict)
-    
-    def find(self, s, wordDict):
-        if s in self.memo:
-            return self.memo[s]
-        if s == "":
-            return True
-        for i in wordDict:
-            if s.startswith(i):
-                if self.find(s[len(i):], wordDict):
-                    self.memo[s] = True
-                    return True
-        self.memo[s] = False
-        return False
+        words = set(wordDict)
+        dp = [False] * (len(s)+1)
+        dp[0] = True
+        
+        for i in range(1, len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in words:
+                    dp[i] = True
+                    break
+        return dp[-1]
