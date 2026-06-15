7f965ffa854e4d9b9a4c45bada253128811f545b
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2026-06-15 20:42:29 +0900

    Time: 225 ms (61.36%), Space: 29.1 MB (28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,27 +4,24 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not grid:
-            return
-        M, N = len(grid), len(grid[0]) # row, col
-        islands = 0
-        visited = set()
-        def bfs(r, c):
-            q = collections.deque()
-            q.append((r,c))
-            visited.add((r,c))
-            directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            while q:
-                cur_x, cur_y = q.popleft()
-                for dx, dy in directions:
-                    row, col = cur_x + dx, cur_y + dy
-                    if row in range(M) and col in range(N) and (row, col) not in visited and grid[row][col] == "1":
-                        visited.add((row, col))
-                        q.append((row,col))
-        
-        for i in range(M):
-            for j in range(N):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    bfs(i,j)
-                    islands += 1
-        return islands
+        # visited를 M * N size로 생성
+        # 이중 for문을 돌면서 not visited면 하, 우 탐색하며 visited True로 바꿔줌 (0인 경우만)
+        # 하, 우 못 옮기면 for문 하나 밖으로 나와서 위 for문 돌림 (이때 cnt += 1)
+        M, N = len(grid), len(grid[0])
+        visited = [[False] * N for _ in range(M)]
+        q = collections.deque()
+        cnt = 0
+        mv = [(0,1), (1,0), (0,-1), (-1,0)]
+        for x in range(M):
+            for y in range(N):
+                if grid[x][y] == "1" and not visited[x][y]:
+                    q.append((x, y))
+                    visited[x][y] = True
+                    while q:
+                        cur_x, cur_y = q.popleft()
+                        for mv_x, mv_y in mv:
+                            if 0 <= cur_x + mv_x < M and 0 <= cur_y + mv_y < N and grid[cur_x+mv_x][cur_y+mv_y] == "1" and not visited[cur_x+mv_x][cur_y+mv_y]:
+                                q.append((cur_x+mv_x, cur_y+mv_y))
+                                visited[cur_x+mv_x][cur_y+mv_y] = True
+                    cnt += 1
+        return cnt
