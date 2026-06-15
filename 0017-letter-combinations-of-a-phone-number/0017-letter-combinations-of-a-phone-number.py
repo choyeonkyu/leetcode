1c1f5877bd58734820a57313190c487cb077dcb2
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2026-06-15 20:57:56 +0900

    Time: 0 ms (100%), Space: 12.5 MB (19.32%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -4,25 +4,25 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        if len(digits) == 0:
-            return []
-        ref = {
-            '2': "abc",
-            '3': "def",
-            '4': "ghi",
-            '5': "jkl",
-            '6': "mno",
-            '7': "pqrs",
-            '8': "tuv",
-            '9': "wxyz"
+        digit_map = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
         }
         answer = []
-        def dfs(cur, idx):
-            if idx == len(digits):
-                answer.append(cur)
+        length = len(digits)
+        def backtrack(num, cum):
+            if len(cum) == length:
+                answer.append(cum)
                 return
-            chars = ref[digits[idx]]
-            for i in chars:
-                dfs(cur+i, idx+1)
-        dfs('', 0)
+            
+            temp_let = digit_map[digits[num]]
+            for i in temp_let:
+                backtrack(num+1, cum+i)
+        backtrack(0, "")
         return answer
