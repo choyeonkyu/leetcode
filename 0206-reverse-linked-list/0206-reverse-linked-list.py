8aec3827e0e1c26013181cdaa86206fc05f98ced
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2026-06-30 20:01:11 +0900

    Time: 0 ms (100%), Space: 14.4 MB (30.99%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -9,16 +9,13 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        if not head:
-            return
-        # head는 포인터
+        if not head or not head.next:
+            return head
         prev = None
-        curr = head # 1
-        nxt = head.next # 2
-        while nxt:
-            curr.next = prev # 1 - None
-            prev = curr # 1
-            curr = nxt # 2
-            nxt = nxt.next
-        curr.next = prev
-        return curr
+        cur = head
+        while cur is not None:
+            nxt = cur.next # 3  4
+            cur.next = prev # 2 -> 1    3 -> 2
+            prev = cur # 2  3
+            cur = nxt # 3   4
+        return prev
