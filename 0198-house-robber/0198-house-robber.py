3d3f57d9fa851892f929f3fee04de2ad3d21a374
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2026-09-20 21:03:16 +0900

    Time: 0 ms (100%), Space: 12.4 MB (22.3%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,14 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dp = {} # (index, total) -> maximum money till index
-
-        def backtrack(i):
-            if i >= len(nums):
-                return 0
-            if i in dp:
-                return dp[i]
-
-            dp[i] = max(backtrack(i + 2)+nums[i], backtrack(i+1))
-            return dp[i]
-        return backtrack(0)
+        rec = [float('-inf')] * len(nums)
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return nums[0] if nums[0] > nums[1] else nums[1]
+        rec[0] = nums[0]
+        rec[1] = max(nums[1], rec[0])
+        for i in range(2, len(nums)):
+            rec[i] = max(rec[i-2]+nums[i], rec[i-1])
+        return rec[-1]
