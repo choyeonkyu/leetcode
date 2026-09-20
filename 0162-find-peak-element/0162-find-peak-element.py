a5e21743c71564790f76cf1c5ff78779772314cb
yeonkyu <142777760+choyeonkyu@users.noreply.github.com>
2026-09-20 20:54:14 +0900

    Time: 0 ms (100%), Space: 12.5 MB (18.1%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -4,14 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left, right = 0, len(nums)-1
-        while left < right-1:
-            mid = (left + right)/2
-            if nums[mid] < nums[mid+1]:
-                left = mid
+        start, end = 0, len(nums) - 1 
+        while start <= end:
+            mid = (start + end) // 2
+            if mid > 0 and nums[mid] < nums[mid-1]:
+                end = mid-1
+            elif mid < len(nums)-1 and nums[mid] < nums[mid+1]:
+                start = mid+1
             else:
-                right = mid
-        if nums[left] < nums[right]:
-            return right
-        else:
-            return left
+                return mid
